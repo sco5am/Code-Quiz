abcaea565908974ab2b9b15a00cefcfdfd46c448
Sam Oaks <samuelclayoaks@yahoo.com>
2022-12-20 19:30:53 -0500

    made several changes to script.js, finally got quiz to cycle through correctly

--- assests/script.js
+++ assests/script.js
@@ -1,169 +1,133 @@
-var questionsEl = document.querySelector('.questions')
-var timerEl = document.querySelector('.time')
-var startBtn = document.querySelector('.start')
-var optionsEl = document.querySelector('.options')
-var initials = document.querySelector('.initials')
-var submitEl = document.querySelector('.submit')
-var feedbackEl = document.querySelector('.feedback')
-var startScreenEl = document.querySelector('.start-screen')
-var titleEl = document.querySelector('.question-title')
-var endScreenEl = document.querySelector('.end-screen');
-var finalScoreEl = document.querySelector('.final-score');
-
-var time = Questions.length*30;
 var questionIndex = 0;
+var totalAnswer = 5;
 
-function StartQuiz() {
+var questionsEl = document.getElementById("questions");
+var choicesEl = document.getElementById("choices");
+var startBtn = document.getElementById("start");
+var feedbackEl = document.getElementById("feedback");
 
-startScreenEl.setAttribute('class', 'hide');
+function startQuiz() {
+  // after pushing button it hides the start screen
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
 
-questionsEl.removeAttribute('class', 'hide');
+  questionsEl.removeAttribute("class");
 
-timerEl = setInterval(clockTick, 1000);
-
-timerEl.textcontent =time;
-
-Question();
+  //   after the start screen is hidden it calls the questions function to load
+  askQuestion();
 }
 
-function Question() {
+function askQuestion() {
+  var currentQuestion = quizQuestions[questionIndex];
 
-    var QuestionPrompt = Questions[questionIndex];
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.question;
 
-    titleEl.textContent = QuestionPrompt.title;
+  choicesEl.innerHTML = "";
+//loop through avalible questions
+  for (var i = 0; i < currentQuestion.multipleChoice.length; i++) {
+    var choice = currentQuestion.multipleChoice[i];
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
 
-    optionsEl.innerHTML = '';
+    choiceNode.textContent = choice;
 
-    for (var i = 0; i < QuestionPrompt.options.length; i++) {
-        var option = QuestionPrompt.options[i];
-        var optionBtn = document.createElement('button');
-        optionBtn.setAttribute('class', 'option');
-        optionBtn.setAttribute('value', option)
-
-        optionBtn.textContent = i + 1 + '.' + option;
-
-        optionsEl.appendChild(optionBtn);
-    }
+    choicesEl.appendChild(choiceNode);
+  }
 }
 
-function optionSelect(event) {
-    var buttonEl = event.target;
-  
-    if (buttonEl !== optionsEl) {
-      return;
-    }
-  
-    if (buttonEl.value !== Questions[questionIndex].answer) {
-      time -= 30;
-  
-      if (time < 0) {
-        time = 0;
-      }
-  
-      timerEl.textContent = time;
-  
-      feedbackEl.textContent = 'Incorrect';
-    } else {
-      
-      feedbackEl.textContent = 'Correct!';
-    }
-    
-    feedbackEl.setAttribute('class', 'feedback');
-    setTimeout(function () {
-      feedbackEl.setAttribute('class', 'feedback hide');
-    }, 2000);
-  
-   
-    questionIndex++;
-  
-    if (time < 0 || questionIndex === Questions.length) {
-      endQuiz();
-    } else {
-      Question();
-    }
+function pickAnswer(event) {
+  var buttonEl = event.target;
+
+  // if the clicked element is not a choice button, do nothing.
+  if (!buttonEl.matches(".choice")) {
+    return;
   }
 
-  function runClock() {
-    time--;
-    timerEl.textContent = time;
-  
-    if (time < 0) {
-      endQuiz();
-    }
+  // check if user guessed wrong or correct
+  if (buttonEl.value !== quizQuestions[questionIndex].correctAnswer) {
+    feedbackEl.textContent = "Wrong!";
+    totalAnswer--;
+  } else {
+    feedbackEl.textContent = "Correct!";
   }
 
-  function endQuiz() {
-    clearInterval(timerId);
-    endScreenEl.removeAttribute('class');
-    
-    finalScoreEl.textContent = time;
-  
-    questionsEl.setAttribute('class', 'hide');
+  // flash right/wrong feedback on page for a second
+  feedbackEl.setAttribute("class", "feedback text-center");
+  setTimeout(function () {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+
+  // move to next question
+  questionIndex++;
+
+  // check if we've run out of questions
+  if (questionIndex === quizQuestions.length) {
+    results();
+  } else {
+    askQuestion();
   }
-
- 
-
-  startBtn.onclick = StartQuiz;
-
-
-
-var Questions = [
-{
-    title: 'Which of the following is NOT an event?',
-    options: [
-        'Keydown',
-        'Change',
-        'Click',
-        'Function',
-    ],
-    answer: "Function",
-},
- {
-    title: 'what Does API stand for?',
-    options:[
-        'Application Programming Interface',
-        'Always Plug Information',
-        'Application Pre-intrigration',
-        'Anytime Programming Instruction',
-    ],
-    answer: "Application Programming Interface",
- },  
-
- {
-    title: 'Commonly used data types include:',
-    options: [
-        'Numbers',
-        'Booleans',
-        'Strings',
-        'All of the above',
-    ],
-    answer: 'All of the above',
- },  
-
- {
-    title: 'String values must be enclosed within ____ when being assigned to variables.',
-    options: [
-        '< or > signs',
-        'quotes',
-        'square brackets',
-        'parentheses',
-    ],
-    answer: 'quotes'
- },  
-
- {
-    title: 'The condition in an if / else statement is enclosed within ____.',
-    options: [
-        'square brackets',
-        'quotes',
-        'parentheses',
-        '< or > signs',
-    ],
-    answer: 'parenthese',
- },  
+}
 
 
 
 
+startBtn.addEventListener("click", startQuiz);
+choicesEl.addEventListener("click", pickAnswer);
 
-]
+var quizQuestions = [
+  {
+      question: 'Which of the following is NOT an event?',
+      multipleChoice: [
+          'Keydown',
+          'Change',
+          'Click',
+          'Function',
+      ],
+      correctAnswer: "Function",
+  },
+   {
+      question: 'what Does API stand for?',
+      multipleChoice: [
+          'Application Programming Interface',
+          'Always Plug Information',
+          'Application Pre-intrigration',
+          'Anytime Programming Instruction',
+      ],
+      correctAnswer: "Application Programming Interface",
+   },  
+  
+   {
+      question: 'Commonly used data types include:',
+      multipleChoice: [
+          'Numbers',
+          'Booleans',
+          'Strings',
+          'All of the above',
+      ],
+      correctAnswer: 'All of the above',
+   },  
+  
+   {
+      question: 'String values must be enclosed within ____ when being assigned to variables.',
+      multipleChoice: [
+          '< or > signs',
+          'quotes',
+          'square brackets',
+          'parentheses',
+      ],
+      correctAnswer: 'quotes'
+   },  
+  
+   {
+      question: 'The condition in an if / else statement is enclosed within ____.',
+      multipleChoice: [
+          'square brackets',
+          'quotes',
+          'parentheses',
+          '< or > signs',
+      ],
+      correctAnswer: 'parenthese',
+   },  
+  ]
