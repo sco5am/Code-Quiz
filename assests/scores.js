c7bc5a6de822109bcae3250d4ff3c2ddcec1b19a
Sam Oaks <samuelclayoaks@yahoo.com>
2022-12-23 15:00:54 -0500

    added descriptions for js and made a few changes

--- assests/scores.js
+++ assests/scores.js
@@ -1,23 +1,27 @@
 function printHighscores() {
-    // either get scores from localstorage or set to empty array
-    var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+    // this will either get our scores from the localstorage or set to empty array
+    let highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
   
     for (var i = 0; i < highscores.length; i += 1) {
-      // create li tag for each high score
-      var liTag = document.createElement('li');
-      liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
+      // this creates list items for each high score
+      let createLi = document.createElement('li');
+      createLi.textContent = highscores[i].initials + ' --- ' + highscores[i].score;
+
   
-      // display on page
-      var olEl = document.getElementById('highscores');
-      olEl.appendChild(liTag);
+      // this will append each list item
+      let olEl = document.getElementById('highscores');
+      olEl.appendChild(createLi);
+
     }
   }
   
   function clearHighscores() {
+    //this will allow the user to clear scores in local storage
     window.localStorage.removeItem('highscores');
     window.location.reload();
   }
-  
-  //document.getElementById('clear').onclick = clearHighscores;
+
+  let clearEl = document.getElementById('clear');
+  clearEl.addEventListener('click',clearHighscores )
   
   printHighscores();
--- assests/script.js
+++ assests/script.js
@@ -1,9 +1,12 @@
+// here we set our question Index to be ready to accept a value
 let questionIndex = 0;
+// here we set out total time for the start of the quiz
 let time = 100;
 let timerId;
 
+// here we get our html elements by their Ids to be used later on in the code 
 let questionsEl = document.getElementById("questions");
-let choicesEl = document.getElementById("choices");
+let optionsEl = document.getElementById("options");
 let startBtn = document.getElementById("start");
 let feedbackEl = document.getElementById("feedback");
 let endScreenEl = document.getElementById("end-screen");
@@ -13,6 +16,7 @@ let initialsEl = document.getElementById("initials");
 let submitBtn = document.getElementById('submit');
 
 function tickClock() {
+  //causes the time to decrease, also runs the results function if the time is equal to or less than 0
   time--;
   timerEl.textContent = time;
   if (time <= 0) {
@@ -21,7 +25,8 @@ function tickClock() {
 }
 
 function startQuiz() {
-  // after pushing button it hides the start screen
+  // after pushing button it hides the start screen then displays the questions.
+  // this function also sets the time and runs the clockTick function to decrease the time
   let startScreenEl = document.getElementById("start-screen");
   startScreenEl.setAttribute("class", "hide");
 
@@ -31,27 +36,30 @@ function startQuiz() {
 
   timerEl.textContent = time;
 
-  //   after the start screen is hidden it calls the questions function to load
+  // This will run the askQuestion function
   askQuestion();
 }
 
 function askQuestion() {
+  // this will display a question as well as its choices from the question Index.
   let currentQuestion = quizQuestions[questionIndex];
 
   let titleEl = document.getElementById("question-title");
   titleEl.textContent = currentQuestion.question;
 
-  choicesEl.innerHTML = "";
-  //loop through avalible questions
+  optionsEl.innerHTML = "";
+  //this loops through avalible questions
   for (var i = 0; i < currentQuestion.multipleChoice.length; i++) {
-    let choice = currentQuestion.multipleChoice[i];
-    let choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", choice);
 
-    choiceNode.textContent = choice;
+    //creates buttons for each choice, so that the users selection can be registered
+    let options = currentQuestion.multipleChoice[i];
+    let optionNode = document.createElement("button");
+    optionNode.setAttribute("class", "options");
+    optionNode.setAttribute("value", options);
 
-    choicesEl.appendChild(choiceNode);
+    optionNode.textContent = options;
+
+    optionsEl.appendChild(optionNode);
   }
 }
 
@@ -59,12 +67,13 @@ function pickAnswer(event) {
   let buttonEl = event.target;
 
   // if the clicked element is not a choice button, do nothing.
-  if (!buttonEl.matches(".choice")) {
+  if (!buttonEl.matches(".options")) {
     return;
   }
 
   // check if user guessed wrong or correct
   if (buttonEl.value !== quizQuestions[questionIndex].correctAnswer) {
+    //if the user guessed wrong, they will get the text Wrong as feedback and be deducted 20 seconds
     feedbackEl.textContent = "Wrong!";
     time -= 20;
     if (time < 0) {
@@ -72,11 +81,12 @@ function pickAnswer(event) {
     }
     timerEl.textContent = time;
   } else {
-    feedbackEl.textContent = "Correct!";
+    //if the user selects correctly, they will get the text Correct as feedback and not lose any time
+    feedbackEl.textContent = 'Correct!';
   }
 
-  // flash right/wrong feedback on page for a second
-  feedbackEl.setAttribute("class", "feedback text-center");
+  // flash right/wrong feedback on page for 2 seconds
+  feedbackEl.setAttribute("class", "feedback");
   setTimeout(function () {
     feedbackEl.setAttribute("class", "feedback hide");
   }, 2000);
@@ -84,7 +94,7 @@ function pickAnswer(event) {
   // move to next question
   questionIndex++;
 
-  // check if we've run out of questions
+  // check if we've run out of questions or time, if so then we get our results, if not we run the askQuestion function again
   if (questionIndex === quizQuestions.length || time < -0) {
     results();
   } else {
@@ -93,48 +103,53 @@ function pickAnswer(event) {
 }
 
 function results() {
+  //This will remove the hide class from the end screen and display the remaining time as the final score
   endScreenEl.removeAttribute("class");
   finalScoreEl.textContent = time;
+  //this will his the question element and clear the remaining time
   questionsEl.setAttribute("class", "hide");
   clearInterval(timerId);
 }
 
 function saveScore() {
-  // get value of input box
-  var initials = initialsEl.value.trim();
+  // this will get what the user inputs into the initials box
+  let initials = initialsEl.value;
 
   // make sure value wasn't empty
   if (initials !== '') {
-    // get saved scores from localstorage, or if not any, set to empty array
-    var highscores =
+    // this will get the highscores from the local storage, or return an empty array
+    let highscores =
       JSON.parse(window.localStorage.getItem('highscores')) || [];
 
-    // format new score object for current user
-    var newScore = {
+    // this will create a score object for the user
+    let score = {
       score: time,
       initials: initials,
     };
 
-    // save to localstorage
-    highscores.push(newScore);
+    // now we are seting the users score to local storage
+    highscores.push(score);
     window.localStorage.setItem('highscores', JSON.stringify(highscores));
 
-    // redirect to next page
+    // this will redirect the user to the Highscores page
     window.location.href = 'Highscore.html';
   }
 }
 
 function checkForEnter(event) {
-  // "13" represents the enter key
+  // this will ensure that if the user presses enter, that it will register and run the saveScore function
   if (event.key === 'Enter') {
     saveScore();
   }
 }
+// Here we add Event listeners that check for a users click on each button, then rus the correct function
 startBtn.addEventListener("click", startQuiz);
-choicesEl.addEventListener("click", pickAnswer);
+optionsEl.addEventListener("click", pickAnswer);
 submitBtn.addEventListener('click', saveScore);
+// this checks for the checkForEnter function when a user releases the Enter key on the initials input
 initialsEl.onkeyup = checkForEnter
 
+//this will create out object that contains all of our questions, options, and the correct answers
 let quizQuestions = [
   {
     question: "Which of the following is NOT an event?",
